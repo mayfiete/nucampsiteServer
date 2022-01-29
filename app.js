f0b7a4023543053c2a817ab4f34b89f624106546
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-01-29 12:01:18 -0500

    feat: rest api with express, mongodb, mongoose

--- app.js
+++ app.js
@@ -27,7 +27,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+app.use('/partners', partnerRouter); 
+
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite'; 
+
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+  err => console.log(err)
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
