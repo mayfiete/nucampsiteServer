a41695255670900f1c559bb5a136a9041044af2a
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-02-10 06:15:37 -0500

    http and secure communication

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var logger = require('morgan');
 
 const passport = require('passport');
@@ -12,32 +11,6 @@ var router = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
-
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
-
-
-
-app.use(passport.initialize());
-
-
-app.use('/', indexRouter);
-app.use('/users', router);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
 
 const mongoose = require('mongoose');
 
@@ -53,6 +26,39 @@ const connect = mongoose.connect(url, {
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
+
+var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/users', router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
